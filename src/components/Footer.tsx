dc3dfb8f815ae0e9cf5a193dfbf961821cfe6082
Ajasa Shamsudeen <shamsudeenajasa@gmail.com>
2025-07-11 20:03:53 +0100

    phone number updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,7 +9,7 @@ function Footer() {
           <img src="/images/BabtechComputersLogo.png" alt="Babtech Logo" className="h-10 w-auto rounded-lg p-1" />
         </div>
         <div className="text-center md:text-right mt-6 md:mt-0 w-full md:w-auto">
-          <div className="mb-1 text-sm">For more inquiries: <a href="mailto:info@babtechlabs.com" className="underline hover:text-blue-300 focus:text-blue-300 transition-colors">PHONE NUMBER</a></div>
+          <div className="mb-1 text-sm">For more inquiries: <a href="mailto:info@babtechlabs.com" className="underline hover:text-blue-300 focus:text-blue-300 transition-colors">+234 816 817 1837</a></div>
         </div>
       </div>
       <div className="max-w-6xl mx-auto flex flex-col md:flex-row items-center justify-between py-4 px-2 text-xs text-blue-200">
